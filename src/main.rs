6602cbfee7ce2cd5e8f8659979437b5014bfc8fc
Bykov Igor <igorb.dev@websm.io>
2018-12-15 16:29:07 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-extern crate libc;
 extern crate dhclient;
 
 use std::mem::*;
@@ -12,6 +11,29 @@ use dhclient::pcap::*;
 use std::io;
 use std::error::Error;
 
+
+#[no_mangle]
+pub unsafe extern "C" fn read_packet_handler(args: *mut u8, header: *const pcap_pkthdr, packet: *const u8) {
+
+    let mut buf = transmute::<*mut u8, &mut Vec<u8>>(args);
+    let header = *header;
+    let packet = std::slice::from_raw_parts(packet, header.len as usize);
+    buf.extend_from_slice(packet);
+
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn dispatch_handler(args: *mut u8, header: *const pcap_pkthdr, packet: *const u8) {
+
+    let mut callback = transmute::<*mut u8, &mut fn(pcap_pkthdr, &[u8])>(args);
+    let header = *header;
+    let packet = std::slice::from_raw_parts(packet, header.len as usize);
+
+    callback(header, packet);
+
+}
+
+
 #[derive(Debug)]
 struct Pcap {
     iface: CString,
@@ -19,7 +41,8 @@ struct Pcap {
 }
 
 impl Pcap {
-    fn new(iface: CString) -> Self {
+
+    fn new(iface: CString) -> Result<Self, Box<dyn Error>> {
         unsafe {
 
             let error: Vec<u8> = vec![0; PCAP_ERRBUF_SIZE as usize];
@@ -31,12 +54,38 @@ impl Pcap {
                 error.as_ptr() as *mut _
                 );
 
-            Pcap {
-                iface: iface,
-                handle: handle,
+            if handle as usize == 0 {
+
+               Err("pcal_open_live error")?;
+
+            } else {
+
+                Ok(Pcap {
+                    iface: iface,
+                    handle: handle,
+                })
+
             }
 
         }
+    }
+
+    fn set_timeout(&mut self, ms: i32) -> Result<(), Box<dyn Error>> {
+
+        let result = unsafe { pcap_set_timeout(self.handle, ms) };
+
+        if result == 0 { Ok(()) }
+        else { Err("pcap_set_timeout error")? }
+
+    }
+
+    fn set_promisc(&mut self, value: bool) -> Result<(), Box<dyn Error>> {
+
+        let result = unsafe { pcap_set_promisc(self.handle, value.into()) };
+
+        if result == 0 { Ok(()) }
+        else { Err("pcap_set_promisc error")? }
+
     }
 
     fn set_filter(&mut self, filter: impl Into<String>) -> Result<(), Box<Error>> {
@@ -63,19 +112,39 @@ impl Pcap {
 
     }
 
-    fn read_packet(&mut self, mut buf: &mut Vec<u8>) -> Result<(), Box<Error>> {
+    fn read_packet(&self, mut buf: &mut Vec<u8>) -> Result<i32, Box<dyn Error>> {
 
-        let buf_ptr = &buf as *const _  as *mut _;
+        let buf_ptr = buf as *const _  as *mut _;
+        println!("{:?}", buf_ptr);
 
-        unsafe {
+        let count = unsafe {
+            pcap_dispatch(self.handle, 1, Some(read_packet_handler), buf_ptr) 
+        };
 
-            let result = pcap_dispatch(self.handle, 1, Some(packet_handler), buf_ptr);
-
+        match count {
+            -1 => Err("pcap_dispatch error")?,
+            -2 => Err("pcap_dispatch break")?,
+            count @ _ => Ok(count),
         }
 
-        Ok(())
+    }
+
+    fn dispatch(&self, callback: fn(pcap_pkthdr, &[u8])) -> Result<i32, Box<dyn Error>> {
+
+        let callback_ptr = &callback as *const _  as *mut _;
+
+        let count = unsafe {
+            pcap_dispatch(self.handle, 1, Some(dispatch_handler), callback_ptr) 
+        };
+
+        match count {
+            -1 => Err("pcap_dispatch error")?,
+            -2 => Err("pcap_dispatch break")?,
+            count @ _ => Ok(count),
+        }
 
     }
+
 }
 
 
@@ -163,49 +232,6 @@ pub unsafe extern "C" fn packet_handler(args: *mut u8, header: *const pcap_pkthd
 
 fn main() {
 
-    let dev = CString::new("alc0").unwrap();
-
-    let mut pcap = Pcap::new(dev);
-    // pcap.set_filter("udp dst port 68").unwrap();
-    println!("{:?}", pcap);
-
-    let mut buffer = Vec::new();
-
-    println!("read packet");
-    pcap.read_packet(&mut buffer).unwrap();
-
-    println!("{:?}", buffer);
-
-    panic!();
-
-    unsafe {
-
-        let dev = CString::new("alc0").unwrap();
-        let error: Vec<u8> = vec![0; PCAP_ERRBUF_SIZE as usize];
-        let handle = pcap_open_live(dev.as_ptr(), BUFSIZ as i32, 1, 1000, error.as_ptr() as *mut _);
-
-        let net: bpf_u_int32 = uninitialized();
-        let mask: bpf_u_int32 = uninitialized();
-
-        let net_ptr = &net as *const _ as *mut _;
-        let mask_ptr = &mask as *const _ as *mut _;
-
-        let result = pcap_lookupnet(dev.as_ptr(), net_ptr, mask_ptr, error.as_ptr() as *mut _);
-
-        let dhcp_program: bpf_program = uninitialized();
-        let dhcp_program_ptr: *mut bpf_program = &dhcp_program as *const _ as *mut _;
-        let dhcp_filter = CString::new("udp dst port 68").unwrap();
-
-        let result = pcap_compile(handle, dhcp_program_ptr, dhcp_filter.as_ptr(), 0, net);
-        let result = pcap_setfilter(handle, dhcp_program_ptr);
-
-        pcap_dispatch(handle, 10, Some(packet_handler), ptr::null_mut() as *mut _);
-
-
-    }
-
-    panic!();
-
     let mut message = unsafe { zeroed::<DhcpMessage>() };
 
     message.op = 0x01;
@@ -231,8 +257,6 @@ fn main() {
     t.reverse();
 
     options.extend_from_slice(&t);
-
-    println!("{:?}", options);
 
     let message_ptr = &message as *const _ as *mut u8;
     let message_size = size_of::<DhcpMessage>();
