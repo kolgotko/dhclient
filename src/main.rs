28d5ef8c83f7b15dd34f9df039e0bdeed7ba013f
Bykov Igor <igorb.dev@websm.io>
2018-12-16 23:49:49 +0300

    remove call unwrap method

--- src/main.rs
+++ src/main.rs
@@ -106,7 +106,7 @@ pub unsafe extern "C" fn packet_handler(
 fn main() -> Result<(), Box<Error>> {
 
     let mac: String = "e8:03:9a:ce:61:27".split(':').collect();
-    let mac = u64::from_str_radix(&mac, 16).unwrap();
+    let mac = u64::from_str_radix(&mac, 16)?;
     let xid = random_u32(0, u32::max_value());
 
     let mut message = unsafe { zeroed::<DhcpMessage>() };
@@ -145,9 +145,9 @@ fn main() -> Result<(), Box<Error>> {
 
     println!("{:?}", msg_vec);
 
-    let socket = UdpSocket::bind("0.0.0.0:68").unwrap();
-    socket.set_broadcast(true).unwrap();
-    socket.send_to(&msg_vec, "255.255.255.255:67").unwrap();
+    let socket = UdpSocket::bind("0.0.0.0:68")?;
+    socket.set_broadcast(true)?;
+    socket.send_to(&msg_vec, "255.255.255.255:67")?;
 
     let iface = lookupdev()?;
     let mut sniffer = Sniffer::new(iface)?;
